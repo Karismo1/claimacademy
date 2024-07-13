4677f9e77a653e97e771e5c2ab2df89e57939bd3
Hassan Fofana <hfofana2003@gmail.com>
2024-07-13 12:07:19 -0500

    Added Student class to create student objects

---
+++ Labs/Lab1/GradeManager/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeManager
+{
+    public class Student
+    {
+        private string _firstName {  get; set; } // private field names should start with underscore for good practice
+        private string _lastName { get; set; }
+
+        // Constructors: See Constructorsection in week1 code for more info
+        public Student()
+        {
+            //Default constructor  - No arguments/parameters and no code
+        }
+
+        public Student(string firstName, string lastName) //parametered constructor, accepts arguments as parameters(in this case, 2 strings are being accepted
+        {
+            _firstName = firstName; //Set the private field via the parameter passed into public constructor
+            _lastName = lastName;
+        }
+
+        //Getters and setters
+        //Setter
+        public void setFirstName(string firstName)//Settoer can be
+        {
+            _firstName = firstName;
+        }
+
+        public string getFirstName()
+        {
+            return _firstName;
+        }
+
+        public void setLastName(string lastName)
+        {
+            _lastName = lastName;
+        }
+
+        public string getLastName()
+        {
+            return _lastName;
+        }
+
+    }
+}
